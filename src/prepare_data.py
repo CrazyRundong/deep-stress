03887b0574dfdfaeb69e1dbd0990487a3d8edace
CrazyRundong <lrd.001@163.com>
2017-03-21 17:27:58 +0800

    seems that data prepare almost done

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -13,7 +13,7 @@ import re
 comsol_dir = '../Data/comsol_source'
 vst_num = 12  # num of vst per axis
 crop_size = 224  # follow standard CNN input size
-radius_scale_factor = 0.8  # R = grid_size * radius_scale_factor
+radius_scale_factor = 0.4  # R = grid_size * radius_scale_factor
 n_points = int(crop_size * (vst_num + 1) / radius_scale_factor)  # num of points interpolated per axis
 grid_x, grid_y = np.mgrid[0:1:complex(0, n_points), 0:1:complex(0, n_points)]
 circular_div = 16  # K in paper
@@ -25,17 +25,18 @@ def load_and_local_interp(data_set_dir=comsol_dir):
     assert os.path.exists(data_set_dir)
     vst_locate = np.linspace(0., 1., vst_num + 2)[1: -1]
     radius = 1. / (vst_num + 1) / 2. * radius_scale_factor
-    tem_list = []
-    stress_list = []
+    tem_list = []  # the X
+    stress_list = []  # the y
     for fname in os.listdir(data_set_dir):
         fname = os.path.splitext(fname)[0]
         tem_tokens = fname.split('_')
         if tem_tokens[0] == 'stress':
             continue
+        tem_path = os.path.join(data_set_dir, fname + '.txt')
         stress_name = '_'.join(['stress'] + tem_tokens[1:]) + '.txt'
         stress_path = os.path.join(data_set_dir, stress_name)
         assert os.path.exists(stress_path)
-        tem_data = np.loadtxt(os.path.join(data_set_dir, fname + '.txt'), usecols=(0, 1, 3), dtype=np.float32)
+        tem_data = np.loadtxt(tem_path, usecols=(0, 1, 3), dtype=np.float32)
         stress_data = np.loadtxt(stress_path, usecols=(0, 1, 3), dtype=np.float32)
         tem_data[:, :2] *= 100
         stress_data[:, :2] *= 100
@@ -45,6 +46,18 @@ def load_and_local_interp(data_set_dir=comsol_dir):
                 current_tem = tem_data[
                     np.logical_and(np.logical_and((x - radius) <= tem_data[:, 0], tem_data[:, 0] <= (x + radius)),
                                    np.logical_and((y - radius) <= tem_data[:, 1], tem_data[:, 1] <= (y + radius)))]
+                current_tem[:, :2] -= (x, y)
+                current_tem[:, :2] /= 2 * radius
+                # rescan tem distribution coordinate
+                roi_bool_idx = np.abs(current_tem[:, :2]) <= 0.2
+                extended_roi_idx = np.hstack((roi_bool_idx,
+                                             np.zeros((current_tem.shape[0], 1), dtype=bool)))
+                extended_not_roi_idx = np.hstack((np.logical_not(roi_bool_idx),
+                                                 np.zeros((current_tem.shape[0], 1), dtype=bool)))
+                current_tem[extended_roi_idx] *= 2.
+                current_tem[extended_not_roi_idx] *= 1. / 3.
+                current_tem[extended_not_roi_idx] += .4 - 1. / 6.
+
                 current_stress = stress_data[
                     np.logical_and(np.logical_and((x - radius) <= stress_data[:, 0], stress_data[:, 0] <= (x + radius)),
                                    np.logical_and((y - radius) <= stress_data[:, 1], stress_data[:, 1] <= (y + radius)))]
@@ -67,7 +80,7 @@ def load_and_interp(data_set_dir=comsol_dir):
         data = np.loadtxt(os.path.join(data_set_dir, fname), usecols=(0, 1, 3), dtype=np.float32)
         points = data[:, :2] * 100  # m to cm
         values = data[:, -1]
-        c = griddata(points, values, (grid_x, grid_y), method='cubic', fill_value=0.)
+        c = griddata(points, values, (grid_x, grid_y), method='cubic', fill_value=values.mean())
         c[c < 0.] = 0.
         current_max = c.max()
         if name_tokens[0] == 'stress':
