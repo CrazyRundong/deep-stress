fix mean calculation

src/prepare_data.py
@@ -2,10 +2,9 @@
 import mxnet as mx
 import numpy as np
 from scipy.interpolate import griddata
-from scipy.ndimage.interpolation import rotate
 from datetime import datetime
 import os
-import re
+from sklearn.model_selection import train_test_split
 
 comsol_dir = './Data/comsol_source'
 data_path = './Data/comsol_crops.npz'
@@ -47,15 +46,6 @@ def load_and_crop(data_set_dir=comsol_dir):
                                    np.logical_and((y - radius) <= tem_data[:, 1], tem_data[:, 1] <= (y + radius)))]
                 current_tem[:, :2] -= (x, y)
                 current_tem[:, :2] /= 2 * radius
-                # rescan tem distribution coordinate
-                # roi_bool_idx = np.abs(current_tem[:, :2]) <= 0.2
-                # extended_roi_idx = np.hstack((roi_bool_idx,
-                #                              np.zeros((current_tem.shape[0], 1), dtype=bool)))
-                # extended_not_roi_idx = np.hstack((np.logical_not(roi_bool_idx),
-                #                                  np.zeros((current_tem.shape[0], 1), dtype=bool)))
-                # current_tem[extended_roi_idx] *= 2.
-                # current_tem[extended_not_roi_idx] *= 1. / 3.
-                # current_tem[extended_not_roi_idx] += .4 - 1. / 6.
 
                 current_stress = stress_data[
                     np.logical_and(np.logical_and((x - radius) <= stress_data[:, 0], stress_data[:, 0] <= (x + radius)),
@@ -78,18 +68,25 @@ def local_interp(tem_list, stress_list, npz_dir=data_path):
     interped_list = np.array(interped_list)
     stress_list = np.array(stress_list)
 
-    tem_mean = np.mean(interped_list, axis=0)
-    interped_list -= tem_mean
-    tem_max = np.abs(interped_list).max()
-    interped_list /= tem_max
+    # Train / Val split
+    temp_train, temp_val, stress_train, stress_val = train_test_split(interped_list, stress_list, train_size=0.7)
 
-    stress_max = stress_list.max()
-    stress_list /= stress_max
+    tem_mean = np.mean(temp_train, axis=0)
+    temp_train -= tem_mean
+    temp_val -= tem_mean
+    tem_max = np.abs(temp_train).max()
+    # interped_list /= tem_max
+
+    stress_max = stress_train.max()
+    stress_train /= stress_max
+    stress_val /= stress_max
 
     # Dump npz
     np.savez_compressed(npz_dir,
-                        temp=interped_list,
-                        stress=stress_list,
+                        temp_train=temp_train,
+                        temp_val=temp_val,
+                        stress_train=stress_train,
+                        stress_val=stress_val,
                         max_temp=tem_max,
                         max_stress=stress_max)
 

tools/predict_via_mx.py
@@ -4,7 +4,6 @@ import src.build_Nets as Net
 
 import mxnet as mx
 import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from matplotlib import pyplot as plt
 
@@ -13,16 +12,16 @@ import logging
 logging.getLogger().setLevel(logging.DEBUG)
 
 
-def main():
-    batch_size = 10
+def main(draw_net=False):
+    batch_size = 256
     momentum = 0.9
-    num_epoch = 5
-    val_ratio = 0.3
+    num_epoch = 100
     disp_batch = 50
-    num_batch = 5
+    num_batch = 1
+    num_plot = 25
 
     base_lr = 0.01
-    lr_rescan_epoch = 500
+    lr_rescan_epoch = 250
     lr_rescan_factor = 0.9
 
     data_path = './Data/comsol_crops.npz'
@@ -30,16 +29,24 @@ def main():
     if not os.path.exists(data_path):
         Data.main()
     data_set = np.load(data_path)
-    x = data_set['temp']
-    y = data_set['stress']
+    x_train = data_set['temp_train']
+    x_val = data_set['temp_val']
+    y_train = data_set['stress_train']
+    y_val = data_set['stress_val']
     max_temp = data_set['max_temp']
     max_stress = data_set['max_stress']
-    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=val_ratio, random_state=0)
 
     train_iter = Data.generate_mx_array_itr(x_train, y_train, batch_size)
     val_iter = Data.generate_mx_array_itr(x_val, y_val, batch_size, shuffle_=False)
+
     # net = Net.build_lenet()
     net = Net.build_net_a()
+
+    # show model
+    if draw_net:
+        g = mx.viz.plot_network(net, shape={'data': (batch_size, 1,) + x_train.shape[-2:], 'reg_label': (batch_size,)})
+        g.render()
+
     metric = mx.metric.RMSE()
     lr_sch = mx.lr_scheduler.FactorScheduler(step=lr_rescan_epoch, factor=lr_rescan_factor)
 
@@ -49,9 +56,6 @@ def main():
         data_names=['data'],
         label_names=['reg_label']
     )
-    # model.bind(data_shapes=train_iter.provide_data,
-    #            label_shapes=train_iter.provide_label)
-    # model.init_params(mx.initializer.Normal())
     model.fit(
         train_iter,
         eval_data=val_iter,
@@ -65,13 +69,16 @@ def main():
     for idx_, score_ in score:
         print('Module {} score: {}'.format(idx_, score_))
 
-    gt_stress = y_val[:batch_size * num_batch]
+    plot_idx = np.random.choice(batch_size * num_batch, num_plot, replace=False)
     predict_stress = model.predict(val_iter, num_batch)
-    mse = mean_squared_error(gt_stress * max_stress, predict_stress.asnumpy() * max_stress)
+    gt_stress = y_val[plot_idx]
+    predict_stress = predict_stress.asnumpy()[plot_idx]
+
+    mse = mean_squared_error(gt_stress * max_stress, predict_stress * max_stress)
     print("MSE: {}".format(mse))
-    plt.plot(range(batch_size * num_batch),
+    plt.plot(range(num_plot),
              gt_stress * max_stress, 'go',
-             predict_stress.asnumpy() * max_stress, 'rx')
+             predict_stress * max_stress, 'rx')
     plt.show()
 
 if __name__ == '__main__':